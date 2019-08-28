098f1c84df90f02b02041238ee3a61c24cd14c52
SimonTakman <simontakman@gmail.com>
2019-08-28 11:29:35 +0200

    Updated button to include knobs

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { withKnobs, object } from "@storybook/addon-knobs/react";
+import { withKnobs, text } from "@storybook/addon-knobs/react";
 
 import { Button } from "./Button";
 
 storiesOf("Button", module)
   .addDecorator(withKnobs)
   .add("default", () => <Button>hej</Button>)
-  .add("long text", () => <Button>This is a very long text</Button>);
+  .add("with knobs", () => <Button>{text("Label", "defaultValue")}</Button>);
